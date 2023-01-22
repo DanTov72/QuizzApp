d262a6c86bba1e8beb5680e1f35776b5885b94a3
DanTov73 <daniarermatov244@gmail.com>
2023-01-22 17:00:46 +0600

    Initial commit

---
+++ app/src/main/java/com/example/quizzapp/presenatation/ui/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.quizzapp.presenatation.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.quizzapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
